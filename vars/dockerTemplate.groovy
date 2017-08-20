23c472339a60c7af1a31965b4e3d5406249a81de
Carlos Sanchez <carlos@apache.org>
2017-08-20 19:12:44 +0200

    Add a docker template

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -3,18 +3,18 @@ def call(Map parameters = [:], body) {
 
   def cloud = parameters.get('cloud', 'kubernetes')
   // ensure agents are not reused
-  def defaultLabel = 'maven-' + currentBuild.projectName + "-" + currentBuild.id
+  def defaultLabel = 'docker-' + currentBuild.projectName + "-" + currentBuild.id
   def label = parameters.get('label', defaultLabel)
-  def image = parameters.get('image', 'maven:latest')
+  def image = parameters.get('image', 'docker:latest')
 
   podTemplate(cloud: cloud, label: label,
     containers: [
-      containerTemplate([name: 'maven', image: "${image}", command: 'cat', ttyEnabled: true,
+      containerTemplate([name: 'docker', image: "${image}", command: 'cat', ttyEnabled: true,
         envVars: [
           envVar(key: '_JAVA_OPTIONS', value:'-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1')
         ],
         resourceLimitMemory: '1024Mi'])],
-    volumes: []) {
+    volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
 
       body()
   }
